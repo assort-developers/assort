48a6f2f41fb40798aaa981679d3b8ef43f2b0fd4
CreateAnything434 <b7067@oic.jp>
2018-11-16 15:18:06 +0900

    注文画面CSS反映２

--- app/Http/Controllers/RecievedController.php
+++ app/Http/Controllers/RecievedController.php
@@ -14,7 +14,7 @@ class RecievedController extends Controller
     public function index()
     {
         //
-        return view('Recieved/recieved_search');
+        return view('recieved/recieved');
 
     }
 
