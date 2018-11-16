5e5d3cbebede2b610c65c734f8018bcfc3abe9c1
むらとお <murato@murakamitooru-no-MacBook-Pro.local>
2018-11-16 15:41:13 +0900

    チケット141小カテゴリのサイドメニューノルーティングとcss反映

---
+++ resources/views/category/category_search.blade.php
@@ -0,0 +1,47 @@
+@extends('layout.common')
+@section('title', '小カテゴリ管理画面')
+@section('header_title', '小カテゴリ管理画面')
+
+@section('content')
+	<div class="content_wrapper">
+	<table class="table-bordered">
+		<tbody>
+		<tr>
+			<th colspan="2">小カテゴリ情報</th>
+		</tr>
+		<tr>
+		<th class="left">カテゴリコード</th>
+		<td></td>
+		</tr>
+		<tr>
+			<th class="left">カテゴリ名</th>
+			<td><input class="form-control" type="text" name="" size="40" maxlength="40" value="" placeholder="カテゴリ名"></td>
+		</tr>
+		<tr>
+			<th class="left">親カテゴリ</th>
+			<td><input class="form-control" type="text" name="" size="40" maxlength="40" value="" placeholder="親カテゴリ名"></td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<th class="left">更新者</th>
+			<td><input class="form-control" type="text" name="" size="40" maxlength="40" value="" placeholder="OIC 太郎"></td>
+		</tr>
+		<tr>
+			<th class="left">更新日</th>
+			<td><input class="form-control" type="date" name="date" value="2018-01-01"></td>
+		</tr>
+		</tbody>
+	</table>
+	<div class="controll_buttons">
+	<a href="#" class="square_btn btn">
+		<i class="fa fa-caret-right"></i>登録</a>
+	<a href="#" class="reseto_btn btn">
+		<i class="fa fa-caret-right"></i>リセット</a> 
+	<a href="#" class="update_btn btn">
+		<i class="fa fa-caret-right"></i>更新</a>
+	<a href="#" class="delete_btn btn">
+		<i class="fa fa-caret-right"></i>削除</a>
+	</div>
+</div>
+@endsection
