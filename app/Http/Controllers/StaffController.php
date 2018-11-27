dc909d78eb9fdaa93e7f0d52dc99742d2479e4e9
okaasandesu <b7070@oic.jp>
2018-11-27 23:15:00 +0900

    staffテーブルにカラム追加し直した

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Staff;
 
 class StaffController extends Controller
 {
--- database/migrations/2018_11_27_215820_add_column_staff_table.php
+++ database/migrations/2018_11_27_215820_add_column_staff_table.php
@@ -14,9 +14,13 @@ class AddColumnStaffTable extends Migration
     public function up()
     {
         Schema::table('staff', function (Blueprint $table) {
-            $table->date('birthday')->after('first_name_kana');
-            $table->date('hiredate')->after('mail_address');
-
+            $table->date('birthday')->default(NULL)->after('first_name_kana');
+            $table->date('hiredate')->default(NULL)->after('mail_address');
+            $table->string('zip_code', 8)->after('birthday');
+            $table->integer('address_pref')->after('zip_code');
+            $table->string('address_city', 45)->after('address_pref');
+            $table->string('address_town', 100)->after('address_city');
+            $table->string('address_build', 100)->nullable()->default(NULL)->after('address_town');
         });
     }
 
@@ -30,6 +34,11 @@ class AddColumnStaffTable extends Migration
         Schema::table('staff', function (Blueprint $table) {
             $table->dropColumn('birthday');
             $table->dropColumn('hiredate');
-        });
+            $table->dropColumn('zip_code');
+            $table->dropColumn('address_pref');
+            $table->dropColumn('addres_city');
+            $table->dropColumn('addres_town');
+            $table->dropColumn('addres_bild');
+        });    
     }
 }
