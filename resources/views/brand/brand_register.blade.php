284ca6228f8bf3739627201d1f5d3a51b249c17b
okaasandesu <b7070@oic.jp>
2018-11-27 14:32:10 +0900

    ブランド管理のバリデーション以外は完成

--- resources/views/brand/brand_register.blade.php
+++ resources/views/brand/brand_register.blade.php
@@ -14,30 +14,30 @@
 				<tr>
 					<th>ブランドコード</th>
 					<td colspan="3">
-						<div class="search_box"><input class="form-control" type="text" name="brand_code" value=""></div>
+						<div class="search_box"><input class="form-control" type="text" name="brand_code" value="" required></div>
 					</td>
 				</tr>
 				<tr>
 					<th>ブランド名</th>
 					<td colspan="3">
-						<div class="search_box"><input class="form-control" type="text" name="brand_name" value=""></div>
+						<div class="search_box"><input class="form-control" type="text" name="brand_name" value="" required></div>
 					</td>
 				</tr>
 				<tr>
 				<tr>
 					<th>ブランド名（カナ）</th>
-					<td colspan="3"><input class="form-control" type="text" name="brand_kana" size="40" maxlength="60" name="brand_kana" value=""></td>
+					<td colspan="3"><input class="form-control" type="text" name="brand_kana" size="40" maxlength="60" name="brand_kana" value="" required></td>
 				</tr>
 				</tr>
 				<tr>
 					<th class="left">郵便番号</th>
 					<td class="row">
 						<div class="col-xs-2">
-							<input class="form-control" type="tel" name="zip_code1" size="3" maxlength="3" value="">
+							<input class="form-control" type="tel" name="zip_code1" size="3" maxlength="3" value="" required>
 						</div>
 						<div class="hyphen">-</div>
 						<div class="col-xs-2">
-							<input class="form-control" type="tel" name="zip_code2" size="4" maxlength="4"value="">
+							<input class="form-control" type="tel" name="zip_code2" size="4" maxlength="4"value="" required>
 						</div>
 				</tr>
 				<tr>
@@ -51,45 +51,45 @@
 					</td>
 					<th class="left">市区町村</th>
 					<td>
-						<input class="form-control" type="text" name="address_city" size="40" maxlength="40" value="">
+						<input class="form-control" type="text" name="address_city" size="40" maxlength="40" value="" required>
 					</td>
 				</tr>
 				<tr>
 					<th class="left">番地</th>
-					<td><input class="form-control" type="text" name="address_town" size="40" maxlength="40" value=""></td>
+					<td><input class="form-control" type="text" name="address_town" size="40" maxlength="40" value="" required></td>
 					<th class="left">建物名</th>
 					<td><input class="form-control" type="text" name="address_build" size="40" maxlength="40" value=""></td>
 				</tr>
 				<tr>
 					<th class="left">メールアドレス</th>
 					<td>
-						<input class="form-control" type="email" name="mail" size="20" maxlength="40" value="">
+						<input class="form-control" type="email" name="mail" size="20" maxlength="40" value="" required>
 					</td>
 					<th class="left">電話番号</th>
 					<td class="row">
-                        <div class="col-xs-3"><input class="form-control" type="tel" name="tel1" size="4" maxlength="4" value="">
+                        <div class="col-xs-3"><input class="form-control" type="tel" name="tel1" size="4" maxlength="4" value="" required>
                         </div>
 						<div class="hyphen">-</div>
-                        <div class="col-xs-3"><input class="form-control" type="tel" name="tel2" size="4" maxlength="4" value="">
+                        <div class="col-xs-3"><input class="form-control" type="tel" name="tel2" size="4" maxlength="4" value="" required>
                         </div>
 						<div class="hyphen">-</div>
-                        <div class="col-xs-3"><input class="form-control" type="tel" name="tel3" size="4" maxlength="4" value=""></div>
+                        <div class="col-xs-3"><input class="form-control" type="tel" name="tel3" size="4" maxlength="4" value="" required></div>
 					</td>
 				</tr>
 				<tr>
 					<th class="left">FAX</th>
 					<td class="row">
-						<div class="col-xs-3"><input class="form-control" type="tel" name="fax1" size="4" maxlength="4" value="">
+						<div class="col-xs-3"><input class="form-control" type="tel" name="fax1" size="4" maxlength="4" value="" required>
 						</div>
 						<div class="hyphen">-</div>
-						<div class="col-xs-3"><input class="form-control" type="tel" name="fax2" size="4" maxlength="4" value="">
+						<div class="col-xs-3"><input class="form-control" type="tel" name="fax2" size="4" maxlength="4" value="" required>
 						</div>
 						<div class="hyphen">-</div>
-						<div class="col-xs-3"><input class="form-control" type="tel" name="fax3" size="4" maxlength="4" value=""></div>
+						<div class="col-xs-3"><input class="form-control" type="tel" name="fax3" size="4" maxlength="4" value="" required></div>
                     </div>
 					</td>
 					<th class="left">社内担当者</th>
-					<td><input class="form-control" type="text" name="staff_id" size="40" maxlength="40" value=""></td>
+					<td><input class="form-control" type="text" name="staff_id" size="40" maxlength="40" value="" required></td>
 				</tr>
 				<tr>
 					<th class="left">更新者</th>
