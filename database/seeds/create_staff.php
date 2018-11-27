e99cc32191bc1180f4965b86193a19425dfb7ddf
okaasandesu <b7070@oic.jp>
2018-11-27 22:38:25 +0900

    seeds/create_staff.phpに項目を追加

--- database/seeds/create_staff.php
+++ database/seeds/create_staff.php
@@ -13,12 +13,14 @@ class create_staff extends Seeder
     {
         DB::table('staff')->insert([
             'code' => 'tyokinuhata',
-            'family_name' => 'テスト',
-            'first_name' => '太郎',
-            'family_name_kana' => 'テスト',
-            'first_name_kana' => 'タロウ',
+            'family_name' => '明仁',
+            'first_name' => '天皇',
+            'family_name_kana' => 'あきひと',
+            'first_name_kana' => 'てんのう',
+            'birthday'=>'1933-12-23',
             'contact_tel' => '080-1111-2222',
             'mail_address' => 'info@koukyo.jp',
+            'hiredate'=>'1989-01-07',
             'staff_role_id' => 1
         ]);
     }
